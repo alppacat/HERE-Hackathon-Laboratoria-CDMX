cc90f562cb714e0134874b26f72c26b46e725d4b
alppacat <lorena.pratts@gmail.com>
2018-10-30 11:48:54 -0600

    Updates

---
+++ precache-manifest.d585ea1930a898aa857311c478c95e2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb2d52771e9ec560225d346c8ad58c86",
+    "url": "/HERE-Hackathon-Laboratoria-CDMX/static/media/school.bb2d5277.png"
+  },
+  {
+    "revision": "ae929c5a982722a23b55",
+    "url": "/HERE-Hackathon-Laboratoria-CDMX/static/js/runtime~main.ae929c5a.js"
+  },
+  {
+    "revision": "de6a8cf585c1b3e43063",
+    "url": "/HERE-Hackathon-Laboratoria-CDMX/static/js/main.de6a8cf5.chunk.js"
+  },
+  {
+    "revision": "4f896a78a6e1625406da",
+    "url": "/HERE-Hackathon-Laboratoria-CDMX/static/js/1.4f896a78.chunk.js"
+  },
+  {
+    "revision": "de6a8cf585c1b3e43063",
+    "url": "/HERE-Hackathon-Laboratoria-CDMX/static/css/main.db2af8d4.chunk.css"
+  },
+  {
+    "revision": "0804524a9bb412d991228c7d1a08121d",
+    "url": "/HERE-Hackathon-Laboratoria-CDMX/index.html"
+  }
+];
