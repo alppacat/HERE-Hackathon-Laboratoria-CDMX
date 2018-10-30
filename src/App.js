73c25f6336fe75fa7c8584b2af8749f61b89d803
RocioMatias05 <rocioa.matias@gmail.com>
2018-10-30 01:30:23 -0600

    Subiendo últimos cambios en maquetación

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Geolocation from './components/Geolocation';
 import './App.css';
 import SignIn from './components/SignIn';
+import Home from './components/Home';
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         </header> */}
         <SignIn/>
         <Geolocation/>
+        <Home/>
       </div>
     );
   }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,14 +5,14 @@ import { Button, NavItem } from "react-materialize";
 
 const Home = () => {
     return (
+
             <NavItem>
             <Button floating large className="teal" waves='ligth' icon='person'></Button>,
             <Button floating large className="teal" waves='ligth' icon='alarm'></Button>,
             <Button floating large className="teal" waves='ligth' icon='map'></Button>,
-            <Button floating large className="teal" waves='ligth' icon='calendary'></Button>
+            <Button floating large className="teal" waves='ligth' icon='dates'></Button>
             </NavItem>
 
-    
     );
 };
 
